0f1989dad813a3ec9bdf208d9154a2ee069f9c9a
JoshuaHLai <cs.joshualai@gmail.com>
2017-02-03 10:03:44 -0800

    Create image_filtering.m

---
+++ Homework 3/image_filtering.m
@@ -0,0 +1,12 @@
+%Import image
+original_image = imread('original_image.jpg');
+
+%Use built-in functions to filter image
+image_filtered_1 = conv2(double(original_image), 1/9 * [1 1 1; 1 1 1; 1 1 1], 'same')
+image_filtered_2 = filter2(1/9 * [1 1 1;1 1 1;1 1 1], double(original_image));
+image_filtered_3 = imfilter(original_image, 1/9 * [1 1 1; 1 1 1; 1 1 1]);
+
+%Display filtered image
+imshow(uint8(image_filtered_1));
+imshow(uint8(image_filtered_2));
+imshow(uint8(image_filtered_3));
