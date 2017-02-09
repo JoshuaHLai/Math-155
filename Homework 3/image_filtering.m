2241fde01e557abe2b14a928bfe6716967bbc206
JoshuaHLai <cs.joshualai@gmail.com>
2017-02-09 14:37:50 -0800

    Update image_filtering.m

--- Homework 3/image_filtering.m
+++ Homework 3/image_filtering.m
@@ -2,7 +2,7 @@
 original_image = imread('original_image.jpg');
 
 %Use built-in functions to filter image
-image_filtered_1 = conv2(double(original_image), 1/9 * [1 1 1; 1 1 1; 1 1 1], 'same')
+image_filtered_1 = conv2(double(original_image), 1/9 * [1 1 1; 1 1 1; 1 1 1], 'same');
 image_filtered_2 = filter2(1/9 * [1 1 1;1 1 1;1 1 1], double(original_image));
 image_filtered_3 = imfilter(original_image, 1/9 * [1 1 1; 1 1 1; 1 1 1]);
 
